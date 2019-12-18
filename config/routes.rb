0ae2bc4d8fa65bbbe3141df87d0002124e77d4d5
Ethan McCue <emccue@live.com>
2019-12-17 22:04:43 -0500

    Add users model and devise
    https://thinkster.io/tutorials/rails-json-api/setting-up-users-and-authentication-for-our-api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'abcd', to: 'root#spa'
+    end
+  end
 
   root 'root#spa'
   get '*route', to: 'root#spa'
