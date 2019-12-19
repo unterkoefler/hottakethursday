03e9754a06a25c1b30e7f15e6a3c803510db8b1a
Ethan McCue <emccue@live.com>
2019-12-19 12:25:16 -0500

    Add active sotrage and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # https://thinkster.io/tutorials/rails-json-api/setting-up-users-and-authentication-for-our-api
+  validates :username,
+            uniqueness: { case_sensitive: false },
+            presence: true,
+            allow_blank: false,
+            format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+
+  def generate_jwt
+    JWT.encode({ id: id,
+                 exp: 60.days.from_now.to_i },
+               Rails.application.secrets.secret_key_base)
+  end
 end
