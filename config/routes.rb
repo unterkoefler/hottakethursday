a889ca720887db2eb3b9df0a3d485018a944436a
Ethan McCue <emccue@live.com>
2019-12-19 21:01:07 -0500

    User login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'abcd', to: 'root#spa'
+      devise_for :users,
+                 controllers: { sessions: 'sessions',
+                                registrations: 'registrations' }
     end
   end
 
