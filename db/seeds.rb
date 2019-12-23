0c90ba012e54d5804f9c4bf7cfe9e825a0c514e3
Ethan McCue <emccue@live.com>
2019-12-23 00:17:46 -0500

    Fix colon rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env == 'development'
-  User.create(email:'example@example.com', password:'123456')
+  User.create(email: 'example@example.com', password: '123456')
 end
