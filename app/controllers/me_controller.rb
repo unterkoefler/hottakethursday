0751cdd2b55e255488f4832be1119929e2ea59ef
Ethan McCue <emccue@live.com>
2019-12-27 13:36:45 -0500

    User routes

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -3,4 +3,14 @@ class MeController < ApplicationController
   def me
     render json: current_user
   end
+
+  def by_id
+    params.require(:id)
+    render json: User.find_by(id: params[:id])
+  end
+
+  def by_ids
+    params.require(:ids)
+    render json: params[:ids].split(',').map { |id| User.find_by(id: id) }
+  end
 end
