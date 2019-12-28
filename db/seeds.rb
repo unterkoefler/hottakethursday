6b3e077a7242465b845ec957937c228e476cee30
unterkoefler <w.unterkoefler@gmail.com>
2019-12-27 20:58:10 -0500

    Seed a couple takes and fix create weirdness

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# If you have already seeded the db and made changes to this file,
+# run `rails db:reset`. Running `rails db:seed` will fail because rails
+# will try to create another user with the same email
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env == 'development'
-  User.create(email: 'example@example.com', password: '123456')
+  u = User.create!(email: 'example@example.com', password: '123456')
+  u.make_the_hottest_of_takes! "Spiderwick Chronicles >> Bridge to Terabithia"
+  u.make_the_hottest_of_takes! "Tea is better lukewarm"
 end
