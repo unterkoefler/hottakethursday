a2293b8001c63109fde1d1420c968fc0db388c26
Ethan McCue <emccue@live.com>
2019-12-29 20:49:36 -0500

    need closing end

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,9 +31,9 @@ class User < ApplicationRecord
 
   def avatar_url
     return nil unless avatar.attached?
-
     url_for avatar
-    
+  end
+
   def make_the_hottest_of_takes!(words)
     Take.create!(contents: words, user: self)
   end
