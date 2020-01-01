f671e4d4a48cc60d3e91218087428e868b0dbbc6
Ethan McCue <emccue@live.com>
2020-01-01 16:26:30 -0500

    Serialize users who liked more cleanly and add field to user for avatar

--- app/serializers/take_serializer.rb
+++ app/serializers/take_serializer.rb
@@ -1,11 +1,5 @@
 class TakeSerializer < ActiveModel::Serializer
   attributes :id, :contents, :created_at, :number_of_likes
   belongs_to :user
-
-  attribute :users_who_liked
-  def users_who_liked
-    object.users_who_liked.map do |u|
-      UserSerializer.new(u)
-    end
-  end
+  has_many :users_who_liked
 end
