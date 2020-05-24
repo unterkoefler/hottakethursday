17b2ff4e5c04bc38dc0c59ac01de4b3e33fe715a
Willy Unterkoefler <w.unterkoefler@gmail.com>
2020-05-24 11:23:07 -0400

    no more cookies

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Hottake
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # No cookies! (http://www.glitchwrks.com/2017/01/16/removing-cookies-sessions-rails-5)
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+
+    config.middleware.use ActionDispatch::Flash
   end
 end
