70fe40e930e24eaf073f27fe8cbb14cfb4635b2d
Willy Unterkoefler <w.unterkoefler@gmail.com>
2020-06-03 22:37:46 -0400

    mail gun instead of sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,17 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hottake_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'hottakethursday.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = {
+    host: 'arcane-mountain-90844.herokuapp.com',
+    protocol: 'https'
+  }
   config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: 'hottakethursday.com',
-    authentication: 'plain',
-    enable_starttls_auto: true
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'arcane-mountain-90844.heroku.com',
+    authentication:  :plain,
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
